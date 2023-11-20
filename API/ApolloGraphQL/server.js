05511646932eb8b38ce6037c91b74e17bf7d73da
cavss <cavss1118@gmail.com>
2023-11-21 00:51:27 +0900

    rocky linux 9 ssh remote connection setting

--- API/ApolloGraphQL/server.js
+++ API/ApolloGraphQL/server.js
@@ -1,21 +1,94 @@
-import { ApolloServer, gql } from "apollo-server"; 
+const { ApolloServer, gql } = require('apollo-server');
+const mariadb = require('mariadb');
+require("dotenv").config();
 
-const resolver_placeList = [
-    {
-        id: "id 1",
-        country: "country 1",
-        category: "category 1",
-        createdDate: "createdDAte 1",
-        title: "title 1"
-    },
-    {
-        id: "id 2",
-        country: "country 2",
-        category: "category 2",
-        createdDate: "createdDAte 2",
-        title: "title 2"
+// Mariadb 연결 정보
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_DATABASE,
+    connectionLimit: process.env.DB_CONNECTION_LIMIT // 연결 수 제한 (선택 사항)
+});
+
+const typeDefs = gql`
+    type JobPortfolio {
+        id : ID!
+        userID : String
+        hasJob : Bool
+        workMonth : Int
+        jobSkill : [String]
+        portfolioURL : [String]
+        certification : [String]
+        languages : [String]
+        education : String
+        workedCompany : [WorkedCompany]
     }
-];
+
+    type WorkedCompany {
+        userID : ID!
+        companyTitle : String
+        workStart : String
+        workEnd : String
+        position : String
+    }
+
+    type Query{
+        readJobPortfolio(userID : String!)
+        readJobPortfolios : [JobPortfolio]
+    }
+`;
+
+const resolvers = {
+    Query : {
+        readJobPortfolio : async () => {
+            let conn;
+            try {
+                conn = await pool.getConnection();
+                const rows = await conn.query('SELECT * FROM users');
+                return rows;
+            } catch (err) {
+                throw err;
+            } finally {
+                if (conn) conn.end();
+            }
+        },
+        readJobPortfolios : async () => {
+            let conn;
+            try {
+                conn = await pool.getConnection();
+                const rows = await conn.query('SELECT * FROM users');
+                return rows;
+            } catch (err) {
+                throw err;
+            } finally {
+                if (conn) conn.end();
+            }
+        },
+    }
+}
+
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+   console.log(`Running on ${url}`); 
+});
+
+CREATE TABLE JobPortfolio (
+    id VARCHAR(36) PRIMARY KEY,
+    userID VARCHAR(255),
+    hasJob BOOLEAN,
+    workMonth INT,
+    jobSkill TEXT, 
+    portfoiloURL TEXT,
+    certification TEXT, 
+    languages TEXT, 
+    education VARCHAR(255)
+);
+
+/*
 
 const resolvers = {
     Query: {
@@ -32,8 +105,19 @@ const resolvers = {
         }
     },
     Mutation : {
-        postMoney() {
-            return [Money]
+        postPlace(_, { title }) {
+            const newPlace = {
+                id : "11",
+                title
+            };
+            resolver_placeList.push(newPlace);
+            return newPlace;
+        },
+        deletePlace(_, { title, id}){
+            const place = resolver_placeList.find((place) => place.id === id);
+            if (!place) return false;
+            resolver_placeList = resolver_placeList.filter((place) => place.id !== place);
+            return true
         }
     }
 };
@@ -94,8 +178,4 @@ const typeDefs = gql `
 
 `;
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen().then(({ url }) => {
-   console.log(`Running on ${url}`); 
-});
+*/
